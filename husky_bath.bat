a99d26ea70f26a8516fdae0bb2361bfd5c0a416a
Zoham <sohamk97@gmail.com>
2019-07-01 02:49:23 +0530

    heroku deployable - removing git fast-forward scripts due to security issues

--- husky_bath.bat
+++
@@ -1,5 +0,0 @@
-@echo "Add, Commit, Push to remote Husky repo"
-CALL git add .
-set /p CommitMessage=What Should We Tell Others Who Own Husky?
-CALL git commit -m %CommitMessage%
-@echo "Husky just took a bath"
